7a63d6c02515d7553a0da4ce2f6dc4d6fcc475b3
coekjan <cn_yzr@qq.com>
2023-11-28 14:28:23 +0800

    rewrites as1 vpn with declaration of all vpns

--- as1.sh
+++ as1.sh
@@ -50,7 +50,35 @@ bgp-rtbr-done 1,2,3,4,5
 
 # vpn
 
-vpn-rtrr 1,2 6 100:6 import=300:1,300:2,400:1,400:2,200:7 export=100:6
-vpn-rtrr-bgp 1,2 6 172.26.15.1
-vpn-rtrr-bind 1,2 6 g0/0/0
-vpn-rtce-bgp 1 6 172.26.15.2,172.26.15.3 202.112.1.0/24
+vpn 1 6
+vpn-rtrr 1,2 100:6 import=300:1,300:2,400:1,400:2,200:7 export=100:6
+vpn-rtrr-bgp 1,2 172.26.15.1
+vpn-rtrr-bind 1,2 g0/0/0
+vpn-rtce-bgp 1 172.26.15.2,172.26.15.3 202.112.1.0/24
+
+vpn 2 7
+vpn-rtrr 1,2 200:7 import=300:1,300:2,400:1,400:2,100:6 export=200:7
+
+vpn 3 1
+vpn-rtrr 1,2 300:1 import=300:1,300:5,400:1,100:6,200:7 export=300:1
+
+vpn 3 2
+vpn-rtrr 1,2 300:2 import=300:2,300:5,400:2,100:6,200:7 export=300:2
+
+vpn 3 3
+vpn-rtrr 1,2 300:3 import=300:3,400:3 export=300:3
+
+vpn 3 5
+vpn-rtrr 1,2 300:5 import=300:1,300:2,400:1,400:2 export=300:5
+
+vpn 4 1
+vpn-rtrr 1,2 400:1 import=300:1,300:5,400:1,100:6,200:7 export=400:1
+
+vpn 4 2
+vpn-rtrr 1,2 400:2 import=300:2,300:5,400:2,100:6,200:7 export=400:2
+
+vpn 4 3
+vpn-rtrr 1,2 400:3 import=300:3,400:3 export=400:3
+
+vpn 4 4
+vpn-rtrr 1,2 400:4 import=300:1,300:2,400:1,400:2 export=400:4
