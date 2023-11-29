2f21fe3d2d9e84d4c3a23c87a82a9089d87a560a
coekjan <cn_yzr@qq.com>
2023-11-29 17:37:06 +0800

    migrates as2

--- as2.sh
+++ as2.sh
@@ -42,7 +42,35 @@ bgp-rtbr-done 1,2,3,4
 
 # vpn
 
-vpn-rtrr 1,2 7 200:7 import=300:1,300:2,400:1,400:2,100:6 export=200:7
-vpn-rtrr-bgp 1,2 7 172.27.15.3
-vpn-rtrr-bind 1,2 7 g0/0/1
-vpn-lsce-bgp 1 7 172.27.15.1,172.27.15.2 172.27.@.0/28
+vpn 1 6
+vpn-rtrr 1,2 100:6 import=300:1,300:2,400:1,400:2,200:7 export=100:6
+
+vpn 2 7
+vpn-rtrr 1,2 200:7 import=300:1,300:2,400:1,400:2,100:6 export=200:7
+vpn-rtrr-bgp 1,2 172.27.15.3
+vpn-rtrr-bind 1,2 g0/0/1
+vpn-lsce-bgp 1 172.27.15.1,172.27.15.2 172.27.@.0/28
+
+vpn 3 1
+vpn-rtrr 1,2 300:1 import=300:1,300:5,400:1,100:6,200:7 export=300:1
+
+vpn 3 2
+vpn-rtrr 1,2 300:2 import=300:2,300:5,400:2,100:6,200:7 export=300:2
+
+vpn 3 3
+vpn-rtrr 1,2 300:3 import=300:3,400:3 export=300:3
+
+vpn 3 5
+vpn-rtrr 1,2 300:5 import=300:1,300:2,400:1,400:2 export=300:5
+
+vpn 4 1
+vpn-rtrr 1,2 400:1 import=300:1,300:5,400:1,100:6,200:7 export=400:1
+
+vpn 4 2
+vpn-rtrr 1,2 400:2 import=300:2,300:5,400:2,100:6,200:7 export=400:2
+
+vpn 4 3
+vpn-rtrr 1,2 400:3 import=300:3,400:3 export=400:3
+
+vpn 4 4
+vpn-rtrr 1,2 400:4 import=300:1,300:2,400:1,400:2 export=400:4
